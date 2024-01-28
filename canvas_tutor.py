f4b25d2565cf9bd180c1358d8c3c655c9dd8b8a6
Ars <xakars02@gmail.com>
2024-01-28 21:24:39 +0700

    add plenty stars

--- canvas_tutor.py
+++ canvas_tutor.py
@@ -1,25 +1,36 @@
 import time
 import curses
 from acync_animation import blink
+import random
 
 
 TIC_TIMEOUT = 0.1
 
+
+def generate_stars_corounes(canvas, row, column, qty=100) -> list:
+    stars = '+*.:'
+    coroutines = []
+    for _ in range(100):
+        rand_row = random.randint(0, row-5)
+        rand_column = random.randint(0, column-5)
+        rand_star = random.choice(stars)
+        coroutines.append(blink(canvas, rand_row, rand_column, rand_star)) 
+    return coroutines   
+
+
 def draw(canvas):
-    row, column = (5, 20)
+    row, column = curses.window.getmaxyx(canvas)
     curses.curs_set(False)
     canvas.border()
-    coroutines = [blink(canvas, row, column+i, '*') for i in range(5)]
+    coroutines = generate_stars_corounes(canvas, row, column, qty=50)
     while True:
         for coroutine in coroutines.copy():
             coroutine.send(None)
             canvas.refresh()
         time.sleep(TIC_TIMEOUT)
 
+
 if __name__=='__main__':
     curses.update_lines_cols()
-    print(curses.window.getmaxyx())
     curses.wrapper(draw)
-
-
 
