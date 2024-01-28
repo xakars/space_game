e78e76f6c74cf142d7b0952b7aa854f799d19301
Ars <xakars02@gmail.com>
2024-01-28 21:39:47 +0700

    add diff blinking

--- acync_animation.py
+++ acync_animation.py
@@ -1,20 +1,22 @@
 import asyncio
 import curses
+import random
 
 
 async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        for _ in range(10):
+        for _ in range(random.randint(1, 15)):
             await asyncio.sleep(0)
+
         canvas.addstr(row, column, symbol)
-        for _ in range(10):
+        for _ in range(random.randint(1, 10)):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        for _ in range(10):
+        for _ in range(random.randint(1, 15)):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        for _ in range(10):
+        for _ in range(random.randint(1, 13)):
             await asyncio.sleep(0)
