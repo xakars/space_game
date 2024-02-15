bde829e4e88d5ed5ba3390057c70a37d2455ac09
Ars <xakars02@gmail.com>
2024-02-15 23:39:27 +0700

    draw space ship

--- canvas_tutor.py
+++ canvas_tutor.py
@@ -1,19 +1,27 @@
 import time
 import curses
-from acync_animation import blink
-from fire_animation import fire
+from acync_animation import blink, animate_spaceship
+from acync_animation import fire
 import random
 
 
 TIC_TIMEOUT = 0.1
+
+with open("./frames/rocket_frame_1.txt", "r") as file:
+    frame1 = file.read()
+
+with open("./frames/rocket_frame_2.txt", "r") as file:
+    frame2 = file.read()
+
+frames = [frame1, frame2]
 
 
 def generate_stars_corounes(canvas, row, column, qty=100) -> list:
     stars = '+*.:'
     coroutines = []
     for _ in range(qty):
-        rand_row = random.randint(0, row-5)
-        rand_column = random.randint(0, column-5)
+        rand_row = random.randint(1, row-1)
+        rand_column = random.randint(1, column-1)
         rand_star = random.choice(stars)
         coroutines.append(blink(canvas, rand_row, rand_column, rand_star)) 
     return coroutines   
@@ -24,7 +32,9 @@ def draw(canvas):
     curses.curs_set(False)
     canvas.border()
     fire_coroutine = fire(canvas, row/2, column/2)
+    space_ship_coroutine = animate_spaceship(canvas, row/2, column/2, frames)
     coroutines = generate_stars_corounes(canvas, row, column, qty=150)
+    coroutines.append(space_ship_coroutine)
     coroutines.append(fire_coroutine)
     while True:
         for coroutine in coroutines.copy():
